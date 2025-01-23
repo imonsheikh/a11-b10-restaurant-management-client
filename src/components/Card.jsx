a13fd98f29f27f09ef50ddc1d10ecf607c3e3374
imon-sheikh <mdimonhossain2017@gmail.com>
2025-01-23 21:15:01 +0600

    implement search functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Card = () => {
+const Card = ({food}) => {
+
+  const {
+    foodName,
+    foodImage,
+    foodCategory,
+    quantity,
+    price,
+    foodOrigin,
+    description} = food
+
     return (
         <div className="bg-white shadow-[0_4px_12px_-5px_rgba(0,0,0,0.4)] w-full py-6 rounded-lg font-[sans-serif] overflow-hidden mx-auto mt-4">
         <div className="flex items-center gap-2 px-6">
-          <h3 className="text-xl text-gray-800 font-bold flex-1">Backlit Keyboard</h3>
+          <h3 className="text-xl text-gray-800 font-bold flex-1">{foodName}</h3>
           <svg xmlns="http://www.w3.org/2000/svg" width="18px" className="cursor-pointer fill-blue-600 shrink-0"
             viewBox="0 0 64 64">
             <path
@@ -14,16 +24,16 @@ const Card = () => {
           </svg>
         </div>
   
-        <div className="min-h-[300px]">
-          <img src="https://readymadeui.com/cardImg.webp" className="w-full my-6" />
+        <div className="h-[220px] ">
+          <img src={foodImage} className="w-full h-full my-6" />
         </div>
   
         <div className="px-6">
-          <p className="text-sm text-gray-600 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Sed auctor.</p>
+          <p className="text-sm text-gray-600 leading-relaxed mt-3"><span className='text-black font-bold'>Desc: </span>{description}</p>
   
           <div className="mt-8 flex items-center flex-wrap gap-4">
-            <h3 className="text-xl text-gray-800 font-bold flex-1">$12.90</h3>
+            <h3 className="text-xl text-gray-800 font-bold flex-1">Price: ${price}</h3>
+            <h3 className="text-xl text-gray-800 font-bold flex-1">Quantity: ${quantity}</h3>
             <Link to='/single-food'>
             <button type="button"
               className="px-5 py-2.5 rounded-lg text-white text-sm tracking-wider bg-blue-600 hover:bg-blue-700 outline-none">Details</button>
--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Heading from '../components/Heading';
 import Card from '../components/Card';
+import axios from 'axios';
 
 const AllFoods = () => {
+
+    const [foods, setFoods] = useState([])
+    const [search, setSearch] = useState('') 
+
+    useEffect(() => {
+        const fetchAllFoods = async () => {
+            const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/all-foods?search=${search}`)
+            console.log(data);
+            setFoods(data) 
+        } 
+        fetchAllFoods() 
+    }, [search])  
+
+    // const handleSearch = (e) => {
+
+    // }
+
     return (
         <div>
+            <div className='bg-slate-400 text-white  pt-2'>
             <Heading title={'All Foods'}></Heading>
+            </div>  
+
+            <div>
+<div class="relative mr-6 my-2">
+  <input  
+  onChange={(e) => setSearch(e.target.value)}
+  type="search" class="bg-purple-white shadow rounded border-2 p-3" placeholder="Search by name..."/>
+  <div class="absolute pin-r pin-t mt-3 mr-4 text-purple-lighter">
+
+
+  </div>
+</div>
+            </div>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5'>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+               {
+                foods.map(food => <Card food={food}></Card>)
+               } 
              </div> 
         </div>
     );
